0a0e2290cecd57a1ee11745a125c86db6d8e16bd
acon701 <50925837+acon701@users.noreply.github.com>
2019-05-24 17:15:18 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,22 +1,4 @@
 
-DIdoMytlbNw6GQHYCEWVqSVdRyLXSeNJ9fYFLzJn
-
-send_grid_key = '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'
-
-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
-
-
-278bcd93f1a35a1d5ace19d4771a5bf7
-
-
-twilio_api_key = 278bcd93f1a35a1d5ace19d4771a5bf7
-twilio_key = 278bcd93f1a35a1d5ace19d4771a5bf7
-
-
-QthqFXxeP5AAAAAAAAAADB0A8Cndj4MAW9CiZE7m-tKLNxxvBK0TUn1NPD2-RThp
-
-dropbox = QthqFXxeP5AAAAAAAAAADB0A8Cndj4MAW9CiZE7m-tKLNxxvBK0TUn1NPD2-RThp
-
-aws_secret_key = 'P/QDG2H4FZUdw+2o29Ioww7BmHxiiYT+DnXqe3h9'
+aws_secret_key = 'DIdoMytlbNw6GQHYCEWVqSVdRyLXSeNJ9fYFLzJn'
 
 aws_access_key_id = 'AKIARBLVSNME4BX6OFM5'
